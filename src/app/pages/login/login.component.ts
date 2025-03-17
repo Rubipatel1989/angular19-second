b861884c42ee948a69f93fbc8e084c4dc987d071
Pawan Kumar <pawank@exeire.com>
2025-03-17 09:15:42 +0530

    LOgin Form Continue

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,59 @@ import { FormsModule } from '@angular/forms';
 })
 export class LoginComponent {
   showRegisterForm = signal<boolean>(false);
-  customerObj: any = {
-    "userId": 0,
-    "userName": "",
-    "emailId": "",
-    "fullName": "",
-    "password": "",
-  };
+  customerObj: Customer = new Customer();
+  loginForm: FormGroup = new FormGroup({
+    userName: new FormControl(),
+    password: new FormControl()
+  });
+
+  http = inject(HttpClient)
   changeView() {
     this.showRegisterForm.set(!this.showRegisterForm());
+  }
+  login() {
+    this.http.post('https://projectapi.gerasim.in/api/BankLoan/Login', this.loginObj).subscribe((res: any) => {
+      if (res.result) {
+        alert("Login Successful");
+      } else {
+        alert(res.message);
+      }
+    }, (error => {
+      console.log(error);
+      console.log(error.message);
+      alert(error.message);
+    }));
+  }
+  register() {
+    this.http.post('https://projectapi.gerasim.in/api/BankLoan/RegisterCustomer', this.customerObj).subscribe((res: any) => {
+      if (res.result) {
+        alert("Registration Successful");
+      } else {
+        alert(res.message);
+      }
+    }, (error => {
+      console.log(error);
+      console.log(error.message);
+      alert(error.message);
+    }));
+  }
+}
+
+
+
+
+export class Customer {
+  userId: number;
+  userName: string;
+  emailId: string;
+  fullName: string;
+  password: string;
+
+  constructor() {
+    this.userId = 0;
+    this.userName = "";
+    this.emailId = "";
+    this.fullName = "";
+    this.password = "";
   }
 }
