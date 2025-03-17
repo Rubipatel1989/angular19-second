368e90c2a11e1877224b3fa4845c4027a3d6dfb5
Pawan Kumar <pawank@exeire.com>
2025-03-17 07:25:47 +0530

    Master

--- README.md
+++ README.md
@@ -65,7 +65,7 @@ For more information on using the Angular CLI, including detailed command refere
 ## Angular 19 study link
 https://www.youtube.com/watch?v=aIAs7XYlPHk
 
-00:24:01
+00:26:10
 
 ## Installation
 1. npm install -g @angular/cli      [First Time need this commond, Also node & npm must installed]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   showRegisterForm = signal<boolean>(false);
-
+  customerObj: any = {
+    "userId": 0,
+    "userName": "",
+    "emailId": "",
+    "fullName": "",
+    "password": "",
+  };
   changeView() {
     this.showRegisterForm.set(!this.showRegisterForm());
   }
