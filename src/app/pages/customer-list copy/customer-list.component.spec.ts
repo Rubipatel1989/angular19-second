45b3c5622458e4cba13b2254ec619aa7275b1a30
Pawan Kumar <pawank@exeire.com>
2025-03-24 19:51:34 +0530

    Add Docker

--- src/app/pages/customer-list copy/customer-list.component.spec.ts
+++ src/app/pages/customer-list copy/customer-list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BankerListComponent } from './banker-list.component';
+import { CustomerListComponent } from './customer-list.component';
 
-describe('BankerListComponent', () => {
-  let component: BankerListComponent;
-  let fixture: ComponentFixture<BankerListComponent>;
+describe('CustomerListComponent', () => {
+  let component: CustomerListComponent;
+  let fixture: ComponentFixture<CustomerListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BankerListComponent]
+      imports: [CustomerListComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(BankerListComponent);
+    fixture = TestBed.createComponent(CustomerListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
